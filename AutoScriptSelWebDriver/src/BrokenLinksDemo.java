2cb245da10f21765973317e8e480e38b82c2ed5a
prashantvaghela <prashantvaghela@localhost>
2021-02-22 23:34:37 -0500

    Find Broken Links

--- AutoScriptSelWebDriver/src/BrokenLinksDemo.java
+++ AutoScriptSelWebDriver/src/BrokenLinksDemo.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 
 public class BrokenLinksDemo {
@@ -31,9 +35,20 @@ public class BrokenLinksDemo {
 		 * Step 1 - get all URL and gets you the status code
 		 * if status code us > 400 then URL is not working -> Link which is tied up to the anchor tag
 		 */
-		String url = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
-		HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+		SoftAssert soft = new SoftAssert();
 		
+		for(WebElement link : links)
+		{
+			String url =link.getAttribute("href");
+			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+			conn.setRequestMethod("HEAD");
+			conn.connect();
+			int respCode = conn.getResponseCode();
+			soft.assertTrue(respCode<400, "The link with text " + link.getText() +" is broken with code "+ respCode);
+		}
+		soft.assertAll();
+		driver.close();
 	}
 
 }
